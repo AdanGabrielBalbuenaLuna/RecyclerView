d340c6e8b8bd38d8b0442386a0f5ffb5ab01a905
Gabriel <agbalbuenaluna@gmail.com>
2021-07-06 03:08:00 -0500

    7 Implemetacion de ViewBinding para list_item que contiene textViews e imageView

--- app/src/main/java/com/example/recyclerview/HeroAdapter.kt
+++ app/src/main/java/com/example/recyclerview/HeroAdapter.kt
@@ -3,7 +3,10 @@ package com.example.recyclerview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.databinding.ListItemBinding
+import com.squareup.picasso.Picasso
 
 //superherolistfa <===> SuperHeroListaForAdapter
 class HeroAdapter (val superherolistfa:List<SuperHero>):RecyclerView.Adapter<HeroAdapter.HeroHolder>(){
@@ -25,9 +28,16 @@ class HeroAdapter (val superherolistfa:List<SuperHero>):RecyclerView.Adapter<Her
 
     //Pinta los list Items
     class HeroHolder(val view:View):RecyclerView.ViewHolder(view){
+
+        //Debido al nombre del list_iem se nombra de este modo
+        val binding = ListItemBinding.bind(view)
+
         fun render(superherolistfa: SuperHero){
+            binding.tvSuperHero.text = superherolistfa.superHero
+            binding.tvRealName.text = superherolistfa.realName
+            binding.tvPublisher.text = superherolistfa.publisher
 
-
+            Picasso.get().load(superherolistfa.image).into(binding.ivHero)
         }
     }
 
